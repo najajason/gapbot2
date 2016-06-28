34153a857a530380e8743e8c60cf6f0aaadc02eb
gapjustin <vuilnisbuilt@gmail.com>
2016-06-28 15:43:32 +0200

    asd

--- app.js
+++ app.js
@@ -3101,7 +3101,7 @@ Dispatcher.sendAction('SEND_MESSAGE', ticketstext)
 }
 var referalarray = []
 if (localStorage.referalarray){
-referalarray = localStorage.referalarray;
+referalarray = [].concat(localStorage.referalarray);
 }
 var alreadyrefered, referalleng
 function referered(referal, referer){
