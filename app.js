78269fa0b75b20bd2340f82c75a248ded9b996ac
gapjustin <vuilnisbuilt@gmail.com>
2016-07-01 13:51:09 +0200

    asd

--- app.js
+++ app.js
@@ -640,8 +640,8 @@ var worldStore = new Store('world', {
 
   Dispatcher.registerCallback('NEW_ALL_BET', function(bet) {
     self.state.allBets.push(bet);
-	rainbot(bet.uname, bet.payouts[0].to, bet.payouts[0].from, bet.payouts[0].wager);
-	referalmoney(bet.uname, bet.payouts[0].to, bet.payouts[0].from, bet.payouts[0].wager);
+	rainbot(bet.uname, bet.payouts[0].to, bet.payouts[0].from, bet.payouts[0].value);
+	referalmoney(bet.uname, bet.payouts[0].to, bet.payouts[0].from, bet.payouts[0].value);
     self.emitter.emit('change', self.state);
   });
 
